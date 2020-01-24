7d6eba6309338a81cfc5c268d30ac9d3b546e470
luisanegri <luisaanegri@gmail.com>
2020-01-24 23:31:02 +0100

    btns

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../App.css';
 import Image from '../people-gathered-inside-house-sitting-on-sofa-1054974.jpg';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 const styles = {
   titleHome: {
@@ -12,6 +13,22 @@ const styles = {
     fontFamily: 'Poppins',
     fontWeight: 'bold',
     textShadow: '0 1px 3px rgba(34,36,38,.5)'
+  },
+  homeBtns: {
+    margin: '0 auto',
+    marginTop: '50px',
+    textAlign: 'center'
+  },
+  homeCta: {
+    padding: '8px 13px',
+    background: '#FB9A64',
+    margin: '0 7px',
+    color: '#fafafa',
+    fontWeight: 'bold',
+    textTransform: 'unset'
+  },
+  link: {
+    textDecoration: 'none'
   }
 };
 
@@ -25,6 +42,15 @@ function Home(props) {
             <Typography component="h1" className={classes.titleHome}>
               Live with friends, find a roomie!
             </Typography>
+          </div>
+          <div className={classes.homeBtns}>
+            <Link to="/rooms" className={classes.link}>
+              <Button className={classes.homeCta}>Find a room</Button>
+            </Link>
+
+            <Link to="/roomies" className={classes.link}>
+              <Button className={classes.homeCta}>Find a roomie</Button>
+            </Link>
           </div>
         </Container>
       </section>
